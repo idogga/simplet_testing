5025a8adba5f0c05158aab6ded60570dfa1bfa1e
petrbihteev <bihtpet581@gmail.com>
2022-08-05 16:33:12 +0700

    Add Integration tests

---
+++ tests/IntegrationTest/IntegrationTest/Controllers/NotificationsController/CompareMethod.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NST.Simple.Api;
+using NUnit.Framework;
+
+namespace IntegrationTest.Controllers.NotificationsController
+{
+    [TestFixture]
+    public class CompareMethod
+    {
+        private readonly HttpClient _httpClient;
+
+        public CompareMethod()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            _httpClient = appFactory.CreateClient();
+        }
+
+        [Test]
+        public async Task Compare_WithMathingValues_ShouldSesccess()
+        {
+            //Arrange
+            int a = 5;
+            int b = 5;
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/api/Simple/{a}/is/{b}");
+
+            //Act
+            var response = await _httpClient.SendAsync(request);
+
+            //Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.Content.ReadAsStringAsync().Result, Is.EqualTo("true"));
+        }
+
+        [Test]
+        public async Task Compare_WithMismatchedValues_ShouldSesccess()
+        {
+            //Arrange
+            int a = 0;
+            int b = 10;
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/api/Simple/{a}/is/{b}");
+
+            //Act
+            var response = await _httpClient.SendAsync(request);
+
+            //Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.Content.ReadAsStringAsync().Result, Is.EqualTo("false"));
+        }
+    }
+}
---
+++ tests/IntegrationTest/IntegrationTest/Controllers/NotificationsController/DoMathMethod.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NST.Simple.Api;
+using NUnit.Framework;
+
+namespace IntegrationTest.Controllers.NotificationsController
+{
+    [TestFixture]
+    public class DoMathMethod
+    {
+        private readonly HttpClient _httpClient;
+
+        public DoMathMethod()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            _httpClient = appFactory.CreateClient();
+        }
+
+        [Test]
+        public async Task DoMath_WithCorrectValues_ShouldSesccess()
+        {
+            //Arrange
+            int a = 100;
+            int b = 70;
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/api/Simple/{a}/plus/{b}");
+
+            //Act
+            var response = await _httpClient.SendAsync(request);
+
+            //Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.Content.ReadAsStringAsync().Result, Is.EqualTo($"{a + b}"));
+        }
+
+        [Test]
+        public async Task DoMath_WithWrong_A_Value_ShouldException()
+        {
+            //Arrange
+            int a = 50;
+            int b = 70;
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/api/Simple/{a}/plus/{b}");
+
+            //Act
+            var response = await _httpClient.SendAsync(request);
+
+            //Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.InternalServerError));
+            Assert.IsTrue(response.Content.ReadAsStringAsync().Result.Contains($"oups,  a value is {a}"));
+        }
+
+        [Test]
+        public async Task DoMath_WithWrong_B_Value_ShouldException()
+        {
+            //Arrange
+            int a = 100;
+            int b = 50;
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/api/Simple/{a}/plus/{b}");
+
+            //Act
+            var response = await _httpClient.SendAsync(request);
+
+            //Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.InternalServerError));
+            Assert.IsTrue(response.Content.ReadAsStringAsync().Result.Contains($"oups, b value is too be {b}"));
+        }
+    }
+}
